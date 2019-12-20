20f0d227db22ae7a960a1a6d34d54c2211896bf7
MattKersey <matthew.l.kersey.18@dartmouth.edu>
2019-12-19 21:56:43 -0500

    Fixed several issues with the appointment editor: date changing now works, the array will sort itself automatically as well

--- Cancer App/Appointments/AppointmentEditor.swift
+++ Cancer App/Appointments/AppointmentEditor.swift
@@ -2,6 +2,8 @@
 //  AppointmentEditor.swift
 //  Oncology Helper
 //
+//  File for facilitating edits to an appointment
+//
 //  Created by Matt Kersey on 12/16/19.
 //  Copyright © 2019 Matt Kersey. All rights reserved.
 //
@@ -9,14 +11,28 @@
 import SwiftUI
 
 struct AppointmentEditor: View {
+/**************************************** Variables ********************************************/
+    
     @EnvironmentObject var userData: UserData
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-    var appointment: Appointment
+    var appointment: Appointment                // Temporary storage for possible cancellation
     
-    var aptIndex: Int? {
-        userData.appointments.firstIndex(where: {$0.id == appointment.id})
+    var aptIndex: Int? {                        // Index of the appointment in the array
+        // Check to see if something went wrong, if so dismiss this view
+        if let index = userData.appointments.firstIndex(where: {$0.id == appointment.id}) {
+            return index
+        } else {
+            self.presentationMode.wrappedValue.dismiss()
+            return nil
+        }
     }
     
+/**************************************** Functions ********************************************/
+    
+    /*
+     Function for cancelling an edit and returning to initial values
+     TODO: Make this actually work
+     */
     func cancel() -> Void {
         userData.appointments[aptIndex!].doctor = appointment.doctor
         userData.appointments[aptIndex!].location = appointment.location
@@ -24,30 +40,39 @@ struct AppointmentEditor: View {
         self.presentationMode.wrappedValue.dismiss()
     }
     
+/**************************************** Main View ********************************************/
+    
     var body: some View {
         List {
-            if (aptIndex != nil) {
+            // Doctor name field
+            HStack {
+                Text("Doctor")
+                    .font(.headline)
+                Divider()
+                TextField("Doctor", text: $userData.appointments[aptIndex!].doctor)
+            }
+            // Location name field
+            HStack {
+                Text("Location")
+                    .font(.headline)
+                Divider()
+                TextField("Location", text: $userData.appointments[aptIndex!].location)
+            }
+            // Date picker for appointment
+            DatePicker(selection: $userData.appointments[aptIndex!].date, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+            
+            // Cancel button
+            Button(action: {self.cancel()}) {
                 HStack {
-                    Text("Doctor")
-                        .font(.headline)
-                    Divider()
-                    TextField("Doctor", text: $userData.appointments[aptIndex!].doctor)
-                }
-
-                HStack {
-                    Text("Location")
-                        .font(.headline)
-                    Divider()
-                    TextField("Location", text: $userData.appointments[aptIndex!].location)
-                }
-                
-                DatePicker(selection: .constant(userData.appointments[aptIndex!].date), label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
-                Button(action: {self.cancel()}) {
+                    Spacer()
                     Text("Cancel")
                         .foregroundColor(.red)
+                    Spacer()
                 }
             }
         }
+        // For ensuring that the array is sorted on a date change
+        .onDisappear(perform: {self.userData.appointments.sort(by: {$0.date < $1.date})})
     }
 }
 
