f1aa54ed27938f077128e49dd464af522c0b891d
MattKersey <matthew.l.kersey.18@dartmouth.edu>
2020-01-07 22:35:22 -0500

    Basic audio playback working

--- Oncology Helper/Appointments/Recordings/AppointmentRecordingPlay.swift
+++ Oncology Helper/Appointments/Recordings/AppointmentRecordingPlay.swift
@@ -8,19 +8,55 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct AppointmentRecordingPlay: View {
 /**************************************** Variables ********************************************/
     
     var appointment: Appointment
     
+    @State var audioPlayer: AVAudioPlayer? = nil
+    
 /**************************************** Functions ********************************************/
+    
+    func play() -> Void {
+        if (audioPlayer == nil) {
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOf: appointment.recordingURL)
+                audioPlayer?.prepareToPlay()
+            } catch {
+                print("audioPlayer was not initialized")
+                return
+            }
+        }
+        audioPlayer!.play()
+    }
+    
+    func setTime(_ timestamp: TimeInterval) {
+        if (audioPlayer == nil) {
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOf: appointment.recordingURL)
+                audioPlayer?.prepareToPlay()
+            } catch {
+                print("audioPlayer was not initialized")
+                return
+            }
+        }
+        audioPlayer!.currentTime = timestamp
+    }
     
     var body: some View {
         List {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            HStack {
+                Button(action: {self.play()}) {
+                    Image(systemName: "play.circle.fill")
+                }
+//                Slider(value: audioPlayer!.currentTime, in: 0.0...audioPlayer!.duration, step: 0.01)
+            }
             ForEach(appointment.timestamps, id: \.self) { timestamp in
-                Text("\(timestamp)")
+                Button(action: {self.setTime(timestamp)}) {
+                    Text("\(timestamp)")
+                }
             }
         }
     }
